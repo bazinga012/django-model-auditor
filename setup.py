f626730a5390e93a52d5935cadfa29f53e9a90fd
vishalagrawal <vishal.agrawal@docon.co.in>
2020-01-17 16:28:47 +0530

    url, email and license

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open("README.md", "r") as fh:
 setup(
     name='django-model-auditor',
     version='0.0.4',
-    url='',
-    license='',
+    url='https://github.com/bazinga012/django-model-auditor',
+    license='MIT',
     author='vishalagrawal',
     install_requires=[
         "Django>=3.0",
         "dictdiffer>=0.8.1"
     ],
     python_requires='>=3.8',
-    author_email='',
     long_description_content_type='text/markdown',
     long_description=long_description,
+    author_email='vishal18593@gmail.com',
     packages=find_packages(),
     description='An extension to the Django web framework that provides version control for model instances.'
 )
